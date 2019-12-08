5b9e11febe038cdb8cbfb881bb135d1541fd9228
jansenv <jansenv44@gmail.com>
2019-12-07 23:56:03 -0600

    basic file structure

---
+++ scripts/photos/PhotoComponent.js
@@ -0,0 +1,6 @@
+const PhotoComponent = (photos) => {
+    return `
+    <div>
+        <img src="${photos}"
+    </div>`
+}
---
+++ scripts/photos/PhotoDataProvider.js
@@ -0,0 +1,5 @@
+let photos = {}
+
+export const usePhotos = () => {
+    return photos
+}
